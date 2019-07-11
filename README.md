58c1b775022d5f5b4bdd22dd2c653d1053a2b5f6
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-07-11 14:26:29 -0400

    add folders again

--- README.md
+++
@@ -1,1 +0,0 @@
-# eventListenersDrill-ben-patrick
--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-$('.thumbnail').click(event => {
-  const newSrc = $(event.target).attr('src');
-  const newAlt = $(event.target).attr('alt');
-  $('.hero img').attr('src', newSrc);
-  $('.hero img').attr('alt', newAlt);
-});
